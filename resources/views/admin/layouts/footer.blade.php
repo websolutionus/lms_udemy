dcb043d43e94051471b5f88bb20766d026f311ed
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-09-09 17:05:25 +0600

    Admin Panel - Mastering Dashboard Template (Part - 2)

---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,20 @@
+<footer class="footer footer-transparent d-print-none">
+    <div class="container-xl">
+        <div class="row text-center align-items-center flex-row-reverse">
+            <div class="col-lg-auto ms-lg-auto">
+                
+            </div>
+            <div class="col-12 col-lg-auto mt-3 mt-lg-0">
+                <ul class="list-inline list-inline-dots mb-0">
+                    <li class="list-inline-item">
+                        Copyright &copy; {{ date('Y') }}
+                        <a href="." class="link-secondary">Tabler</a>.
+                        All rights reserved.
+                    </li>
+                    <li class="list-inline-item">
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
