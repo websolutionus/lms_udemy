8a23cf3c0a8a5f658799e648b860ef6de0562da1
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-09-09 11:01:43 +0600

    Multi Auth - Admin Auth Reset Password

--- app/Http/Controllers/Admin/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Admin/Auth/PasswordResetLinkController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Password;
@@ -15,7 +16,7 @@ class PasswordResetLinkController extends Controller
      */
     public function create(): View
     {
-        return view('auth.forgot-password');
+        return view('admin.auth.forgot-password');
     }
 
     /**
@@ -32,8 +33,15 @@ class PasswordResetLinkController extends Controller
         // We will send the password reset link to this user. Once we have attempted
         // to send the link, we will examine the response then see the message we
         // need to show to the user. Finally, we'll send out a proper response.
-        $status = Password::sendResetLink(
-            $request->only('email')
+        $status = Password::broker('admins')->sendResetLink(
+            $request->only('email'),
+            function($user, $token) {
+                $notification = new ResetPassword($token);
+                $notification->createUrlUsing(function() use ($user, $token) {
+                    return route('admin.password.reset', ['token' => $token, 'email' => $user->email]);
+                });
+                $user->notify($notification);
+            }
         );
 
         return $status == Password::RESET_LINK_SENT
