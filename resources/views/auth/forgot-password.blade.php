a85a8661506037e90666e6312a1ad4ca8bf0c491
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-09-10 18:34:44 +0600

    Frontend Panel - Reset Password Pages

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,59 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
+@extends('frontend.layouts.master')
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+@section('content')
+    <!--===========================
+        SIGN IN START
+    ============================-->
+    <section class="wsus__sign_in">
+        <div class="row align-items-center">
+            <div class="col-xxl-5 col-xl-6 col-lg-6 wow fadeInLeft">
+                <div class="wsus__sign_img">
+                    <img src="{{ asset('frontend/assets/images/login_img_1.jpg') }}" alt="login" class="img-fluid">
+                    <a href="index.html">
+                        <img src="{{ asset('frontend/assets/images/logo.png') }}" alt="EduCore" class="img-fluid">
+                    </a>
+                </div>
+            </div>
+            <div class="col-xxl-4 col-xl-5 col-lg-6 col-md-9 m-auto wow fadeInRight">
+                <div class="wsus__sign_form_area">
+                    <div class="tab-content" id="pills-tabContent">
+                        <div class="tab-pane fade show active" id="pills-home" role="tabpanel"
+                            aria-labelledby="pills-home-tab" tabindex="0">
+                            <!-- Session Status -->
+                            <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
+                            <form action="{{ route('password.email') }}" method="POST">
+                                @csrf
+                                <h2>Forget Password<span>!</span></h2>
+                                <p class="new_user">Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.</p>
+                                <div class="row">
+                                    <div class="col-xl-12">
+                                        <div class="wsus__login_form_input">
+                                            <label>Email *</label>
+                                            <input type="email" name="email" value="{{ old('email') }}" placeholder="Email" required>
+                                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                        </div>
+                                    </div>
+                                         
+                                    <div class="col-xl-12">
+                                        <div class="wsus__login_form_input">
+                                            
+                                            <button type="submit" class="common_btn">Email Password Reset Link</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                            <p class="create_account">Don't have an account? <a href="{{ route('register') }}">Create free
+                                    account</a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+        <a class="back_btn" href="index.html">Back to Home</a>
+    </section>
+    <!--===========================
+        SIGN IN END
+    ============================-->
+@endsection
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,39 +1,73 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('password.store') }}">
-        @csrf
+@extends('frontend.layouts.master')
 
-        <!-- Password Reset Token -->
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+@section('content')
+    <!--===========================
+            SIGN IN START
+        ============================-->
+    <section class="wsus__sign_in">
+        <div class="row align-items-center">
+            <div class="col-xxl-5 col-xl-6 col-lg-6 wow fadeInLeft">
+                <div class="wsus__sign_img">
+                    <img src="{{ asset('frontend/assets/images/login_img_1.jpg') }}" alt="login" class="img-fluid">
+                    <a href="index.html">
+                        <img src="{{ asset('frontend/assets/images/logo.png') }}" alt="EduCore" class="img-fluid">
+                    </a>
+                </div>
+            </div>
+            <div class="col-xxl-4 col-xl-5 col-lg-6 col-md-9 m-auto wow fadeInRight">
+                <div class="wsus__sign_form_area">
+                    <div class="tab-content" id="pills-tabContent">
+                        <div class="tab-pane fade show active" id="pills-home" role="tabpanel"
+                            aria-labelledby="pills-home-tab" tabindex="0">
+                            <x-auth-session-status class="mb-4" :status="session('status')" />
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                            <form action="{{ route('password.store') }}" method="POST">
+                                @csrf
+                                <h2>Reset Password<span>!</span></h2>
+                                <p class="new_user">Welcome Back, please provide your credentials for login</p>
+
+                                <!-- Password Reset Token -->
+                                <input type="hidden" name="token" value="{{ $request->route('token') }}">
+
+                                <div class="row">
+                                    <div class="col-xl-12">
+                                        <div class="wsus__login_form_input">
+                                            <label>Email *</label>
+                                            <input type="email" name="email" value="{{ old('email', $request->email) }}"
+                                                placeholder="Email" required>
+                                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-xl-12">
+                                        <div class="wsus__login_form_input">
+                                            <label>Password *</label>
+                                            <input type="password" name="password" required placeholder="Password">
+                                            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-12">
+                                        <div class="wsus__login_form_input">
+                                            <label>Confirm Password *</label>
+                                            <input type="password" name="password_confirmation" required placeholder="Password">
+                                            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                                        </div>
+                                    </div>
+
+                                    <div class="col-xl-12">
+                                        <div class="wsus__login_form_input">
+                                            <button type="submit" class="common_btn">Reset Password</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </section>
+    <!--===========================
+            SIGN IN END
+        ============================-->
+@endsection
