f1e018c45c8b63c70179cb310ed897baebf291e1
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-09-11 11:42:49 +0600

    Instructor Request - Adding Necessary Columns in User Table

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,21 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->enum('role', ['instructor', 'student']);
+            $table->string('image')->default('/default-files/avatar.png');
             $table->string('name');
+            $table->string('headline')->nullable();
             $table->string('email')->unique();
+            $table->text('bio')->nullable();
+            $table->enum('gender', ['male', 'female'])->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['instructor', 'student']);
+            $table->string('facebook')->nullable();
+            $table->string('x')->nullable();
+            $table->string('linkedin')->nullable();
+            $table->string('website')->nullable();
+            $table->string('github')->nullable();
+            $table->enum('login_as', ['student', 'instructor'])->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
