e5c51c6acea58524b638cdd9161969ff4836bca6
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-09-12 12:47:25 +0600

    Instructor Request - Send Email Reject Email to Instructor

--- app/Http/Controllers/Admin/InstructorRequestController.php
+++ app/Http/Controllers/Admin/InstructorRequestController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Mail\InstructorRequestApprovedMail;
+use App\Mail\InstructorRequestRejectMail;
 use App\Models\User;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
@@ -22,67 +23,44 @@ class InstructorRequestController extends Controller
         return view('admin.instructor-request.index', compact('instructorsRequests'));
     }
 
-    function download(User $user) {
-        
+    function download(User $user)
+    {
         return response()->download(public_path($user->document));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, User $instructor_request) : RedirectResponse
+    public function update(Request $request, User $instructor_request): RedirectResponse
     {
         $request->validate(['status' => ['required', 'in:approved,rejected,pending']]);
         $instructor_request->approve_status = $request->status;
         $request->status == 'approved' ? $instructor_request->role = 'instructor' : "";
         $instructor_request->save();
 
-        if(config('mail_queue.is_queue')) {
-            Mail::to($instructor_request->email)->queue(new InstructorRequestApprovedMail());
-        }else {
-            Mail::to($instructor_request->email)->send(new InstructorRequestApprovedMail());
-        }
+        self::sendNotification($instructor_request);
 
         return redirect()->back();
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
+    public static function sendNotification($instructor_request): void
     {
-        //
+        switch ($instructor_request->approve_status) {
+            case 'approved':
+                if (config('mail_queue.is_queue')) {
+                    Mail::to($instructor_request->email)->queue(new InstructorRequestApprovedMail());
+                } else {
+                    Mail::to($instructor_request->email)->send(new InstructorRequestApprovedMail());
+                }
+                break;
+            case 'rejected':
+
+                if (config('mail_queue.is_queue')) {
+                    Mail::to($instructor_request->email)->queue(new InstructorRequestRejectMail());
+                } else {
+                    Mail::to($instructor_request->email)->send(new InstructorRequestRejectMail());
+                }
+        }
     }
 }
