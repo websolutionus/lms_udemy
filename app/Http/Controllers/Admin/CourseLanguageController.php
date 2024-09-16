8faefd2cfe402554aee924c81469e09a8a177962
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-09-16 11:29:33 +0600

    3_Coruse Language - Working with Create Feature (Part -2)

--- app/Http/Controllers/Admin/CourseLanguageController.php
+++ app/Http/Controllers/Admin/CourseLanguageController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\CourseLanguage;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Str;
 
 class CourseLanguageController extends Controller
 {
@@ -19,17 +22,26 @@ class CourseLanguageController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create() : View
     {
-        //
+        return view('admin.course.course-language.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request) : RedirectResponse
     {
-        //
+        $request->validate(['name' => ['required', 'max:255', 'unique:course_languages']]);
+
+        $language = new CourseLanguage();
+        $language->name = $request->name;
+        $language->slug = Str::slug($request->name);
+        $language->save();
+
+        notyf()->success('Created Successfully!');
+
+        return to_route('admin.course-languages.index');
     }
 
     /**
--- resources/views/admin/course/course-language/index.blade.php
+++ resources/views/admin/course/course-language/index.blade.php
@@ -7,7 +7,7 @@
                 <div class="card-header">
                     <h3 class="card-title">Course Languages</h3>
                     <div class="card-actions">
-                        <a href="#" class="btn btn-primary">
+                        <a href="{{ route('admin.course-languages.create') }}" class="btn btn-primary">
                             <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
                             <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
                                 viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
