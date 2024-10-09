a49b7f328b7a5cd649ea4fe896c58e1b7c53c949
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-10-09 17:11:42 +0600

    Stripe Setting - Working With Stripe Setting (Part -1)

--- resources/views/admin/payment-setting/index.blade.php
+++ resources/views/admin/payment-setting/index.blade.php
@@ -27,12 +27,12 @@
                         <div class="card-header">
                             <ul class="nav nav-tabs card-header-tabs nav-fill" data-bs-toggle="tabs" role="tablist">
                                 <li class="nav-item" role="presentation">
-                                    <a href="#paypal-seeting" class="nav-link active" data-bs-toggle="tab" aria-selected="false"
+                                    <a href="#paypal-setting" class="nav-link active" data-bs-toggle="tab" aria-selected="false"
                                         role="tab" tabindex="-1">Paypal Settings</a>
                                 </li>
                                 <li class="nav-item" role="presentation">
-                                    <a href="#tabs-profile-5" class="nav-link" data-bs-toggle="tab" aria-selected="false"
-                                        role="tab" tabindex="-1">Profile</a>
+                                    <a href="#stripe-setting" class="nav-link" data-bs-toggle="tab" aria-selected="false"
+                                        role="tab" tabindex="-1">Stripe Settings</a>
                                 </li>
                                 <li class="nav-item" role="presentation">
                                     <a href="#tabs-activity-5" class="nav-link" data-bs-toggle="tab"
@@ -42,7 +42,7 @@
                         </div>
                         <div class="card-body">
                             <div class="tab-content">
-                                <div class="tab-pane active" id="paypal-seeting" role="tabpanel">
+                                <div class="tab-pane active" id="paypal-setting" role="tabpanel">
                                     <form action="{{ route('admin.paypal-setting.update') }}" method="POST">
                                         @csrf
                                         <div class="row">
@@ -111,10 +111,67 @@
                                         </div>
                                     </form>
                                 </div>
-                                <div class="tab-pane" id="tabs-profile-5" role="tabpanel">
-                                    <h4>Profile tab</h4>
-                                    <div>Fringilla egestas nunc quis tellus diam rhoncus ultricies tristique enim at diam,
-                                        sem nunc amet, pellentesque id egestas velit sed</div>
+                                <div class="tab-pane" id="stripe-setting" role="tabpanel">
+                                    <form action="{{ route('admin.paypal-setting.update') }}" method="POST">
+                                        @csrf
+                                        <div class="row">
+                                            <div class="col-md-5">
+                                                <div class="mb-3">
+                                                    <label class="form-label">Stripe Staus</label>
+                                                    <select name="stripe_status" class="form-control" >
+                                                        <option @selected(config('gateway_settings.stripe_status') === 'sandbox') value="sandbox">Sandbox</option>
+                                                        <option @selected(config('gateway_settings.stripe_status') === 'live') value="live">Live</option>
+                                                    </select>
+                                                    <x-input-error :messages="$errors->get('stripe_status')" class="mt-2" />
+                                                </div>
+                                            </div>
+    
+                                            <div class="col-md-5">
+                                                <div class="mb-3">
+                                                    <label class="form-label">Currency</label>
+                                                    <select name="stripe_currency" class="form-control select2" >
+                                                        @foreach(config('gateway_currencies.stripe_currencies') as $key => $value)
+                                                        <option @selected(config('gateway_settings.stripe_currency') == $value)  value="{{ $value }}">{{ $value }}</option>
+                                                        @endforeach
+                                                    </select>
+                                                    
+                                                    <x-input-error :messages="$errors->get('paypal_currency')" class="mt-2" />
+                                                </div>
+                                            </div>
+                                            <div class="col-md-2">
+                                                <div class="mb-3">
+                                                    <label class="form-label">Rate (USD)</label>
+                                                    <input type="text" class="form-control" name="stripe_rate"
+                                                        placeholder="Enter Stripe Rate" value="{{ config('gateway_settings.stripe_rate') }}">
+                                                    <x-input-error :messages="$errors->get('stripe_rate')" class="mt-2" />
+                                                </div>
+                                            </div>
+    
+                                            <div class="col-md-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label">Publishable Key</label>
+                                                    <input type="text" class="form-control" name="stripe_publishable_key"
+                                                        placeholder="Enter Stripe publishable key" value="{{ config('gateway_settings.stripe_publishable_key') }}">
+                                                    <x-input-error :messages="$errors->get('stripe_publishable_key')" class="mt-2" />
+                                                </div>
+                                            </div>
+    
+                                            <div class="col-md-6">
+                                                <div class="mb-3">
+                                                    <label class="form-label">Clinet Secret</label>
+                                                    <input type="text" class="form-control" name="stripe_secret"
+                                                        placeholder="Enter Stripe client secret" value="{{ config('gateway_settings.stripe_secret') }}">
+                                                    <x-input-error :messages="$errors->get('stripe_secret')" class="mt-2" />
+                                                </div>
+                                            </div>
+                            
+    
+                                            
+                                        </div>
+                                        <div class="text-end">
+                                            <button type="submit" class="btn btn-primary">Save</button>
+                                        </div>
+                                    </form>
                                 </div>
                                 <div class="tab-pane show" id="tabs-activity-5" role="tabpanel">
                                     <h4>Activity tab</h4>
