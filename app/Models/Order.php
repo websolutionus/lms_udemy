8f7a2190edc55ea8f52eb00037c317d3a6237480
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-10-13 10:35:57 +0600

    Order - Showing Order Details (Part -2)

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
@@ -14,5 +15,10 @@ class Order extends Model
     function customer() : BelongsTo
     {
         return $this->belongsTo(User::class, 'buyer_id', 'id');    
+    }
+
+
+    function orderItems() : HasMany {
+       return $this->hasMany(OrderItem::class, 'order_id', 'id'); 
     }
 }
--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class OrderItem extends Model
 {
     use HasFactory;
+
+    function course() : BelongsTo {
+       return $this->belongsTo(Course::class, 'course_id', 'id'); 
+    }
 }
