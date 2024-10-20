7acfae26c19884b132329514edac1a570aa0cbab
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-10-20 10:00:12 +0600

    Course Player - Showing Dynamic Contents in Sidebar

--- app/Http/Controllers/Frontend/EnrolledCourseController.php
+++ app/Http/Controllers/Frontend/EnrolledCourseController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Course;
 use App\Models\Enrollment;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
@@ -15,8 +16,10 @@ class EnrolledCourseController extends Controller
         return view('frontend.student-dashboard.enrolled-course.index', compact('enrollments'));     
     }
 
-    function payerIndex() : View
+    function payerIndex(string $slug) : View
     {
-        return view('frontend.student-dashboard.enrolled-course.player-index');     
+        $course = Course::where('slug', $slug)->firstOrFail();
+        if(!Enrollment::where('user_id', user()->id)->where('course_id', $course->id)->where('have_access', 1)->exists()) return abort(404);
+        return view('frontend.student-dashboard.enrolled-course.player-index', compact('course'));
     }
 }
