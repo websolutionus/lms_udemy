1591f96a52508c1c06d1c365a7fd9297f5541ecc
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-10-24 16:25:43 +0600

    Hero Section - Show Dynamic Contents in Fields

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Hero;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class FrontendController extends Controller
 {
    function index() : View {
-    return view('frontend.pages.home.index');
+      $hero = Hero::first();
+    return view('frontend.pages.home.index', compact('hero'));
    } 
 }
--- resources/views/frontend/pages/home/sections/hero-section.blade.php
+++ resources/views/frontend/pages/home/sections/hero-section.blade.php
@@ -1,33 +1,37 @@
-<section class="wsus__banner_3" style="background: url(images/banner_3_bg.png);">
+<section class="wsus__banner_3" style="background: url({{ asset('frontend/assets/images/banner_3_bg.png') }});">
     <div class="row justify-content-between">
         <div class="col-xl-6 col-lg-6 wow fadeInUp">
             <div class="wsus__banner_3_text">
-                <h5>Show Up For Learning</h5>
-                <h1>Premier E-Learning Courses From <span>EduCore</span></h1>
-                <p class="description">Nullam tincidunt tortor est, ac maximus justo gravida non phasellus dignissim
-                    quam odio ipsum
-                    sollicitudin rhoncus venenatis ex metus in turpis.</p>
+                @if($hero->label)
+                <h5>{{ $hero->label }}</h5>
+                @endif
+                <h1>{{ $hero->title }}</h1>
+                <p class="description">{{ $hero->subtitle }}</p>
                 <div class="wsus__banner_2_btn_area mt_60">
-                    <a class="common_btn" href="#">Start Free Trial <i class="far fa-arrow-right"
+                    @if($hero->button_text)
+                    <a class="common_btn" href="{{ $hero->button_url }}">{{ $hero->button_text }} <i class="far fa-arrow-right"
                             aria-hidden="true"></i></a>
+                    @endif
+                    @if($hero->button_text)
                     <div class="play_btn_area">
                         <a class="play_btn venobox vbox-item" data-autoplay="true" data-vbtype="video"
-                            href="https://youtu.be/sVPYIRF9RCQ?si=labNkx-xlyOWtptr">
-                            <img src="images/play_icon.png" alt="Play" class="img-fluid">
+                            href="{{ $hero->video_button_url }}">
+                            <img src="{{ asset('frontend/assets/images/play_icon.png') }}" alt="Play" class="img-fluid">
                         </a>
-                        <h4>See Our Lesson Showcase</h4>
+                        <h4>{{ $hero->video_button_text }}</h4>
                     </div>
+                    @endif
                 </div>
             </div>
         </div>
         <div class="col-xl-4 col-lg-6 wow fadeInRight">
             <div class="wsus__banner_3_img">
                 <div class="img">
-                    <img src="images/banner_3_img_1.png" alt="Banner" class="img-fluid">
+                    <img src="{{ asset($hero->image) }}" alt="Banner" class="img-fluid">
 
                     <div class="text">
-                        <h4>250+ Popular Course</h4>
-                        <p>Explore a variety of fresh topics</p>
+                        <h4>{{ $hero->banner_item_title }}</h4>
+                        <p>{{ $hero->banner_item_subtitle }}</p>
                     </div>
 
                     <div class="circle_box">
@@ -41,7 +45,7 @@
                             </defs>
                             <text>
                                 <textPath xlink:href="#circle">
-                                    take the worldwide best online course
+                                    {{ $hero->round_text }}
                                 </textPath>
                             </text>
                         </svg>
