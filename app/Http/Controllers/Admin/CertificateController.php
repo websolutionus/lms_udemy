6ae3930d760b9d7fb902d04434fedfd8a59babff
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-10-24 10:24:45 +0600

    Certificate Builder - Replacing Certificate Contents

--- app/Http/Controllers/Admin/CertificateController.php
+++ app/Http/Controllers/Admin/CertificateController.php
@@ -15,10 +15,24 @@ class CertificateController extends Controller
     function index() {
         $certificate = CertificateBuilder::first();
         $certificateItems = CertificateBuilderItem::all();
-        $pdf = Pdf::loadView('frontend.student-dashboard.enrolled-course.certificate', compact('certificate', 'certificateItems'))
-        ->setPaper('a4', 'landscape');
+        $html = view('frontend.student-dashboard.enrolled-course.certificate', compact('certificate', 'certificateItems'))->render();
+        
+        $html = str_replace("[student_name]", user()->name, $html);
+        $html = str_replace("[course_name]", user()->name, $html);
+        $html = str_replace("[date]", date('d-m-Y'), $html);
+        $html = str_replace("[platform_name]", 'Edu Core', $html);
+        $html = str_replace("[instructor_name]", user()->name, $html);
+
+
+
+        $pdf = Pdf::loadHTML($html)->setPaper('a4', 'landscape');
+        return $pdf->stream('certificate.pdf');
+
+        // $pdf = Pdf::loadView('frontend.student-dashboard.enrolled-course.certificate', compact('certificate', 'certificateItems'))
+        // ->setPaper('a4', 'landscape');
+        // dd($pdf);
         // return $pdf;    
-        return $pdf->download('certificate.pdf'); 
+        // return $pdf->download('certificate.pdf'); 
     //    return view('frontend.student-dashboard.enrolled-course.certificate', compact('certificate', 'certificateItems')); 
     }
 
