cf50ad4c00104ac5484f5c8e1cf29d85678895bf
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-10-27 13:32:11 +0600

    About Section - Working with Update (Part -3)

--- app/Http/Controllers/Admin/AboutUsSectionController.php
+++ app/Http/Controllers/Admin/AboutUsSectionController.php
@@ -4,17 +4,21 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\AboutUsSectionUpdateRequest;
+use App\Models\AboutUsSection;
+use App\Traits\FileUpload;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class AboutUsSectionController extends Controller
 {
+    use FileUpload;
     /**
      * Display a listing of the resource.
      */
     public function index() : View
     {
-        return view('admin.sections.about-section.index');
+        $about = AboutUsSection::first();
+        return view('admin.sections.about-section.index', compact('about'));
     }
 
 
@@ -23,6 +27,41 @@ class AboutUsSectionController extends Controller
      */
     public function store(AboutUsSectionUpdateRequest $request)
     {
+
+       $data = [
+         'rounded_text' => $request->rounded_text,
+         'lerner_count' => $request->lerner_count,
+         'lerner_count_text' => $request->lerner_count_text,
+         'title' => $request->about_title,
+         'description' => $request->about_description,
+         'button_text' => $request->button_text,
+         'button_url' => $request->button_url,
+         'video_url' => $request->lerner_count_text,
+       ];
+
+       if($request->hasFile('image')) {
+           $image = $this->uploadFile($request->file('image'));
+           $this->deleteFile($request->old_image);
+           $data['image'] = $image;
+       }
+
+       if($request->hasFile('lerner_image')) {
+           $image = $this->uploadFile($request->file('lerner_image'));
+           $this->deleteFile($request->old_lerner_image);
+           $data['lerner_image'] = $image;
+       }
+
+       if($request->hasFile('video_image')) {
+           $image = $this->uploadFile($request->file('video_image'));
+           $this->deleteFile($request->old_video_image);
+           $data['video_image'] = $image;
+       }
+
+       AboutUsSection::updateOrCreate(['id' => 1], $data);
+
+       notyf()->success('Updated Successfully');
+
+       return redirect()->back();
         
     }
 }
--- app/Models/AboutUsSection.php
+++ app/Models/AboutUsSection.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class AboutUsSection extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'rounded_text',
+        'lerner_count',
+        'lerner_count_text',
+        'title',
+        'description',
+        'button_text',
+        'button_url',
+        'video_url',
+        'image',
+        'lerner_image',
+        'video_image',
+    ];
 }
