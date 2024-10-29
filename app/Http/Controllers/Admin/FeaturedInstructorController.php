1ee7253f531f5f92b0a20111dc0ec9b255f6a2a9
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-10-29 10:04:32 +0600

    Featured Instructor Section - Working with Featured Instructor Section (Part -2)

--- app/Http/Controllers/Admin/FeaturedInstructorController.php
+++ app/Http/Controllers/Admin/FeaturedInstructorController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class FeaturedInstructorController extends Controller
@@ -12,7 +13,8 @@ class FeaturedInstructorController extends Controller
      */
     public function index()
     {
-        return view('admin.sections.featured-instructor.index');
+        $instructors = User::where('role', 'instructor')->where('approve_status', 'approved')->get();
+        return view('admin.sections.featured-instructor.index', compact('instructors'));
     }
 
     /**
--- resources/views/admin/sections/featured-instructor/index.blade.php
+++ resources/views/admin/sections/featured-instructor/index.blade.php
@@ -5,7 +5,7 @@
         <div class="container-xl">
             <div class="card">
                 <div class="card-header">
-                    <h3 class="card-title">Hero</h3>
+                    <h3 class="card-title">Featured Instructor</h3>
                     
                 </div>
                 <div class="card-body">
@@ -13,21 +13,75 @@
                         @csrf
                        
                         <div class="row">
+
                             <div class="col-md-12">
                                 <div class="mb-3">
-                                    <label class="form-label">Image One</label>
-                                    <input type="file" class="form-control" name="image_one"
-                                        placeholder="">
-                                    <input type="hidden" name="old_image_one" value="">
-                                    <x-input-error :messages="$errors->get('image_one')" class="mt-2" />
+                                    <label class="form-label">Title</label>
+                                    <input type="text" class="form-control" name="title"
+                                        placeholder="" value="">
+                                    <x-input-error :messages="$errors->get('title')" class="mt-2" />
                                 </div>
                             </div>
+
+                            <div class="col-md-12">
+                                <div class="mb-3">
+                                    <label class="form-label">Subtitle</label>
+                                    <textarea name="subtitle" class="form-control"></textarea>
+                                   <x-input-error :messages="$errors->get('title')" class="mt-2" />
+                                </div>
+                            </div>
+
                             <div class="col-md-6">
                                 <div class="mb-3">
-                                    <label class="form-label">Title One</label>
-                                    <input type="text" class="form-control" name="title_one"
+                                    <label class="form-label">Button Text</label>
+                                    <input type="text" class="form-control" name="button_text"
                                         placeholder="" value="">
-                                    <x-input-error :messages="$errors->get('title_one')" class="mt-2" />
+                                    <x-input-error :messages="$errors->get('button_text')" class="mt-2" />
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <div class="mb-3">
+                                    <label class="form-label">Button Url</label>
+                                    <input type="text" class="form-control" name="button_url"
+                                        placeholder="" value="">
+                                    <x-input-error :messages="$errors->get('button_url')" class="mt-2" />
+                                </div>
+                            </div>
+
+                            <div class="col-md-12">
+                                <div class="mb-3">
+                                    <label class="form-label">Instructor</label>
+                                    <select name="instructor" class="select2">
+                                        <option value="">Select</option>
+                                        @foreach ($instructors as $instructor)
+                                        <option value="">{{ $instructor->name }}</option>
+                                        @endforeach
+                                    </select>
+                                    <x-input-error :messages="$errors->get('instructor')" class="mt-2" />
+                                </div>
+                            </div>
+
+                            <div class="col-md-12">
+                                <div class="mb-3">
+                                    <label class="form-label">Courses</label>
+                                    <select name="instructor" class="select2" multiple>
+                                        <option value="">Select</option>
+                                        <option value="">Select</option>
+                                        <option value="">Test</option>
+                                        <option value="">Test 2</option>
+                                    </select>
+                                    <x-input-error :messages="$errors->get('instructor')" class="mt-2" />
+                                </div>
+                            </div>
+
+                            <div class="col-md-12">
+                                <div class="mb-3">
+                                    <label class="form-label">Instructor Image</label>
+                                    <input type="file" class="form-control" name="instructor_image"
+                                        placeholder="">
+                                    <input type="hidden" name="old_instructor_image" value="">
+                                    <x-input-error :messages="$errors->get('instructor_image')" class="mt-2" />
                                 </div>
                             </div>
                            
