1725450ef84f9dc69407289cccf8e87c7c24e3fb
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-11-03 15:51:27 +0600

    10_Course Review - Working With Course Review (Part -4)

--- app/Http/Controllers/Admin/ReviewController.php
+++ app/Http/Controllers/Admin/ReviewController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Review;
+use Exception;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
@@ -18,50 +19,31 @@ class ReviewController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Review $review)
     {
-        //
+        $review->status = $request->status ? 1 : 0;
+        $review->save();
+
+        notyf()->success('Updated Successfully!');
+        return redirect()->back();
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Review $review)
     {
-        //
+        try {
+            $review->delete();
+            notyf()->success('Deleted Successfully!');
+            return response(['message' => 'Deleted Successfully!'], 200);
+        }catch(Exception $e) {
+            logger("Course Ratting Error >> ".$e);
+            return response(['message' => 'Something went wrong!'], 500);
+        }
+
+    
     }
 }
--- resources/views/admin/review/index.blade.php
+++ resources/views/admin/review/index.blade.php
@@ -43,22 +43,24 @@
                                     @endif
                                 </td>
                                 <td>
-                                    <form action="">
-                                    <select name="status" class="form-control">
-                                        <option value="0">Pending</option>
-                                        <option value="1">Approved</option>
+                                    <form action="{{ route('admin.reviews.update', $review->id) }}" method="POST">
+                                        @csrf
+                                        @method('PUT')
+                                    <select name="status" class="form-control" onchange="this.form.submit()">
+                                        <option @selected($review->status == 0) value="0">Pending</option>
+                                        <option @selected($review->status == 1) value="1">Approved</option>
                                     </select>
                                     </form>
                                 </td>
                                 <td>
-                                    <a href="{{ route('admin.course-levels.destroy', $review->id) }}" class="text-red delete-item">
+                                    <a href="{{ route('admin.reviews.destroy', $review->id) }}" class="text-red delete-item">
                                         <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-trash-x"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7h16" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /><path d="M10 12l4 4m0 -4l-4 4" /></svg>
                                     </a>
                                 </td>
                                 </tr>
                                 @empty
                                 <tr>
-                                    <td colspan="3" class="text-center">No Data Found!</td>
+                                    <td colspan="7" class="text-center">No Data Found!</td>
                                 </tr>
                                 @endforelse
                             </tbody>
