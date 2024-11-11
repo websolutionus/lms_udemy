cc327a661bc28b1256efc9910e1e4b693ac9069b
Shakil Ahmed <sakilhossain01969@gmail.com>
2024-11-11 14:21:55 +0600

    SMTP Setting - Setting Config Dynamically

--- app/Providers/SettingServiceProvider.php
+++ app/Providers/SettingServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Service\SettingService;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider;
 
 class SettingServiceProvider extends ServiceProvider
@@ -12,7 +13,7 @@ class SettingServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->singleton(SettingService::class, function() {
+        $this->app->singleton(SettingService::class, function () {
             return new SettingService();
         });
     }
@@ -24,5 +25,18 @@ class SettingServiceProvider extends ServiceProvider
     {
         $settings = $this->app->make(SettingService::class);
         $settings->setGlobalSettings();
+
+
+        // set mail config
+        Config::set('mail.mailers.smtp', [
+            'transport' => config('settings.mail_mailer'),
+            'host' => config('settings.mail_host'),
+            'port' => config('settings.mail_port'),
+            'username' => config('settings.mail_username'),
+            'password' => config('settings.mail_password'),
+            'encryption' => config('settings.mail_encryption'),
+        ]);
+
+        Config::set('mail_queue.is_queue', config('settings.mail_queue'));
     }
 }
